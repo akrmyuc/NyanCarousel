3cd5f5b65e1ca6ba4a37a4eb7932ff0fcf2bf37e
akari <87920616+akrmyuc@users.noreply.github.com>
2022-04-03 19:32:52 +0900

    fix: 最初と最後のボタンを押せないように修正

--- src/main.js
+++ src/main.js
@@ -8,14 +8,32 @@
   const slides = ul.children;
   let currentIndex = 0;
 
+  function updateButtons() {
+    // あとの条件に当てはまらないときは、クラスを外しておく
+    prev.classList.remove("hidden");
+    next.classList.remove("hidden");
+
+    // 当てはまる場合はクラスをつける
+    if (currentIndex === 0) {
+      prev.classList.add("hidden");
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add("hidden");
+    }
+  }
+
+  updateButtons();
+
   next.addEventListener("click", () => {
     currentIndex++;
+    updateButtons();
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
 
   prev.addEventListener("click", () => {
     currentIndex--;
+    updateButtons();
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
